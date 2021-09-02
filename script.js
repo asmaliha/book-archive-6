ac190c2e54fce3500aa53369ebfb5d9eedf854d4
asmaliha <afia.maliha65@gmail.com>
2021-09-02 23:45:20 +0600

    second-commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// Searching book name using onclick
+const searchBooks = () => {
+    const searchBox = document.getElementById('search-box');
+    const textSearched = searchBox.value;
+    // console.log(textSearched);
+
+    // dynamic url of bookname
+    const url = `https://openlibrary.org/search.json?q=${textSearched}`;
+    console.log(url);
+
+    // json conversion
+    fetch(url)
+        .then(res => res.json())
+        // .then(data => console.log(data.docs));
+        .then(data => showSearchResults(data.docs));
+}
+const showSearchResults = docs => {
+    const searchResult = document.getElementById('detail-card');
+    docs.forEach(key => {
+        console.log(key);
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `<img src="https://covers.openlibrary.org/b/id/${key.cover_i}-M.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.</p>
+        </div>
+        <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+        `
+        searchResult.appendChild(div);
+    }
+
+    )
+}
